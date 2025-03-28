98ec5fa9096492a69b933f2f419d29f8779c214c
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:26:46 +0800

    code completion

--- print_string_in_lower_case.py
+++ print_string_in_lower_case.py
@@ -1,3 +1,5 @@
 # Ask for the user to input their full name
 # Add the '.lower()' method to convert the input to lower case
+full_name = input("Enter your full nama: ").lower()
 # Print the input
+print(full_name)
