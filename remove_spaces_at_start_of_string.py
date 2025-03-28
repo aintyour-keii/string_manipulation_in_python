b2baab5c2ed6115256f34f28e84623a40a83c338
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:06:52 +0800

    modifying the input

--- remove_spaces_at_start_of_string.py
+++ remove_spaces_at_start_of_string.py
@@ -1,4 +1,4 @@
 # Ask for user input of their full name
-full_name = input("Please enter your full name: ")
 # Add the '.lstrip()' method at the end, outside of the input parenthesis
+full_name = input("Please enter your full name: ").lstrip()
 # Print the variable that stores the input
