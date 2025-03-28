3a0531262c9542516c9851795798dff6e9e44542
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:22:51 +0800

    created pseudocode

---
+++ ljust_recreation.py
@@ -0,0 +1,7 @@
+# Ask the user to input a string
+# Ask the user for the width
+# Check if string needs extra spaces
+# If so, add spaces to the string, 
+# the number of spaces is equal to that of the width minus the length of the string
+# If not, do nothing
+# Print the result
