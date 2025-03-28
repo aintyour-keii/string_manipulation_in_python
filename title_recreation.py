b654c0b196178e15635efdd42b2fe8e03f3586a3
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:48:58 +0800

    created pseudocode

---
+++ title_recreation.py
@@ -0,0 +1,6 @@
+# Ask the user to input a string
+# Split the string
+# Create a variable to store the modified string
+# Loop throught the parts of the string
+# Use '.capitalize()' on each part and add it onto the modified string variable
+# After looping, print the modified string variable
