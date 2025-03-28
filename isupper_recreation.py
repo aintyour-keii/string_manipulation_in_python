c379aaceec08ceb2d7f73c821a1d607992a93a21
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:08:42 +0800

    pseudocode changes

--- isupper_recreation.py
+++ isupper_recreation.py
@@ -1,5 +1,5 @@
 # Ask the user to input a string
-# Initialize a boolean to track
+# Initialize a boolean to track if the string is in all caps
 # Itterate through the string
 # And check if the character is in range of "a-z" then set the boolean to false and break
 # Print the value of the boolean
