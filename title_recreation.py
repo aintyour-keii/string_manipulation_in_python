8e7fa50a803ba0863d6807a72c7045274ddef6d3
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:55:07 +0800

    code completion

--- title_recreation.py
+++ title_recreation.py
@@ -1,7 +1,16 @@
 # Ask the user to input a string
+user_input = input("Please enter a string: ")
 # Split the string
+words = user_input.split()
 # Create a variable to store the modified string
+modified_string = ""
 # Loop throught the parts of the string
-# Check if the modified string is still empty, then add the first word after using the '.capitalize()' on it
-# if the modified string is not empty, Use '.capitalize()' on each part and add it onto the modified string after a space
+for word in words:
+    # Check if the modified string is still empty, then add the first word after using the '.capitalize()' on it
+    if modified_string == "":
+        modified_string = word.capitalize()
+    else: # if the modified string is not empty, Use '.capitalize()' on each part and add it onto the modified string after a space
+        modified_string += " " + word.capitalize()
+
 # After looping, print the modified string variable
+print(modified_string)
