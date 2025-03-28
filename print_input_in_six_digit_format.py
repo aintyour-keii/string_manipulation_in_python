0ac940f2f3e3c829c60a109a240c4a337742180f
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:19:20 +0800

    number user input

--- print_input_in_six_digit_format.py
+++ print_input_in_six_digit_format.py
@@ -1,3 +1,4 @@
 # Ask the user to input a number from 0-1000
+number = int(input("Enter a number between 0 and 1000: "))
 # Check if the number is valid
 # If valid, use the '.zfill()' method to make the input into a 6 digit number then print the modified input
