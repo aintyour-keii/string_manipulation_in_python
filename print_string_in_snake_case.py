444e7ab1f7f582b50c63bf6dd146d57a253b60d7
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:42:18 +0800

    code completion

--- print_string_in_snake_case.py
+++ print_string_in_snake_case.py
@@ -1,4 +1,6 @@
 # Ask the user to input their full name in incorrect casing
 # Use the '.strip()' and '.lower()' method to remove unecessary spaces and conver the string to lower case
 # Use the '.replace()' to replace the spaces inside the string with underscore
+full_name = input("Enter your full name: ").strip().lower().replace(" ", "_")
 # Print the modified input
+print(full_name)
