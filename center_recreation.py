65553508055c77c58296ec3c92fc1d9a11dcd993
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:30:36 +0800

    created pseudocode

---
+++ center_recreation.py
@@ -0,0 +1,6 @@
+# Ask the user to input a string
+# Ask the user for the width
+# Check if string needs extra spaces
+# If so, calculate the total number of spaces, spaces for the left and right side
+# Add the spaces to left and right side of the string
+# Print the formatted string
