fc04ac77823daf2f2891c292d6e801f99f24d69b
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:37:26 +0800

    created pseudocode

---
+++ print_string_in_pascal_case.py
@@ -0,0 +1,4 @@
+# Ask the user to input their full name in incorrect casing
+# Use the '.title()' to correct the casing
+# Use the '.replace()' to replace the spaces with empty string
+# Print the modified input
