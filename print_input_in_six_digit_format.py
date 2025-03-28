2333b3aaedb0af969456513fb2d3bc7ffeee7914
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:18:44 +0800

    created pseudocode

---
+++ print_input_in_six_digit_format.py
@@ -0,0 +1,3 @@
+# Ask the user to input a number from 0-1000
+# Check if the number is valid
+# If valid, use the '.zfill()' method to make the input into a 6 digit number then print the modified input
