bdb81610b4923ce93ebdfb30a4cbe5bcdb385748
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:53:07 +0800

    created pseudocode

---
+++ removeprefix_recreation.py
@@ -0,0 +1,5 @@
+# Ask for the user input
+# Ask for the prefix to be removed
+# Check if the string starts with the prefix
+# If it does, remove the prefix and print the result
+# If it doesn't, print the string unmodified
