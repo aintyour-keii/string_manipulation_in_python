1ff160653b4e5f7238eb521aa7958958aaafcd8b
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:33:31 +0800

    code completion

--- print_word_count_in_string.py
+++ print_word_count_in_string.py
@@ -1,3 +1,6 @@
 # Ask the user to input a statement
+statement = input("Please enter a statement: ")
 # Split the statement into words using '.split()'
+words = statement.split()
 # Get the length of the returned list of the split method and print it
+print(len(words))
