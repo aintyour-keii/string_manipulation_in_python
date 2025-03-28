eed06adfbbb934796bc6fea53556c8272afcaddf
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:38:43 +0800

    code completion

--- print_string_in_pascal_case.py
+++ print_string_in_pascal_case.py
@@ -1,4 +1,6 @@
 # Ask the user to input their full name in incorrect casing
 # Use the '.title()' to correct the casing
 # Use the '.replace()' to replace the spaces with empty string
+full_name = input("Please enter your full name: ").title().replace(" ", "")
 # Print the modified input
+print(full_name)
