75e2596d26567034585fce6d91b7735fa2782f63
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:24:11 +0800

    modifying the input

--- print_string_in_upper_case.py
+++ print_string_in_upper_case.py
@@ -1,4 +1,4 @@
 # Ask for the user to input their full name
-full_name = input("Enter your full name: ")
 # Add the '.upper()' method to convert the input to uppercase
+full_name = input("Enter your full name: ").upper()
 # Print the input
