3d0a5520a5f48caf542962352291f04bb1ecae64
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:09:54 +0800

    code completion

--- isupper_recreation.py
+++ isupper_recreation.py
@@ -1,5 +1,15 @@
 # Ask the user to input a string
+text_input = input("Enter a string: ")
+
 # Initialize a boolean to track if the string is in all caps
+all_caps = True
+
 # Itterate through the string
-# And check if the character is in range of "a-z" then set the boolean to false and break
+for char in text_input:
+    # And check if the character is in range of "a-z" then set the boolean to false and break
+    if 'a' <= char <= 'z':
+        all_caps = False
+        break
+
 # Print the value of the boolean
+print(all_caps)
