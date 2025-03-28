885213d28b06b81a6991a4221acfdee68cd35938
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:07:25 +0800

    created pseudocode

---
+++ isupper_recreation.py
@@ -0,0 +1,5 @@
+# Ask the user to input a string
+# Initialize a boolean to track
+# Itterate through the string
+# And check if the character is in range of "a-z" then set the boolean to false and break
+# Print the value of the boolean
