527201d4af6d1fab2d37be735d0b554928bea894
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:45:26 +0800

    code completion

--- capitalize_recreation.py
+++ capitalize_recreation.py
@@ -1,5 +1,10 @@
 # Ask the user for a string input
+user_input = input("Please enter a string: ")
 # Use the '.upper()' method on the first character of the input
+first_char_upper = user_input[0].upper()
 # Use the '.lower()' method on the rest of the characters of the input
+rest_chars_lower = user_input[1:].lower()
 # Add these together
+result = first_char_upper + rest_chars_lower
 # Print the result
+print(result)
