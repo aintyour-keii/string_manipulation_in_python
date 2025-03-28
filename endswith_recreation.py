60a1111951bd3112f8e6ac1159f36a73d6136afe
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:17:17 +0800

    code completion

--- endswith_recreation.py
+++ endswith_recreation.py
@@ -1,4 +1,8 @@
 # Ask for the user input
+user_input = input("Enter a string: ")
 # Ask for the suffix to be checked
+suffix = input("Enter a suffix to check: ")
 # Check if the suffix is at the end of the input
+endswith = user_input[-len(suffix):] == suffix
 # If it is, print True. Otherwise, print False
+print(endswith)
