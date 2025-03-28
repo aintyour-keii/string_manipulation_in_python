c1a942e8c4d030acc558d043a86689cc3e523184
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:07:08 +0800

    printing the input

--- remove_spaces_at_start_of_string.py
+++ remove_spaces_at_start_of_string.py
@@ -2,3 +2,4 @@
 # Add the '.lstrip()' method at the end, outside of the input parenthesis
 full_name = input("Please enter your full name: ").lstrip()
 # Print the variable that stores the input
+print(full_name)
