7361bba155763a8170c033b69cfb484dc0eb4aeb
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:44:54 +0800

    created pseudocode

---
+++ capitalize_recreation.py
@@ -0,0 +1,5 @@
+# Ask the user for a string input
+# Use the '.upper()' method on the first character of the input
+# Use the '.lower()' method on the rest of the characters of the input
+# Add these together
+# Print the result
