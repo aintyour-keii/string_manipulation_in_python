eed11a81e92c24894208070274b6d52735f12c41
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:40:50 +0800

    created pseudocode

---
+++ print_string_in_snake_case.py
@@ -0,0 +1,4 @@
+# Ask the user to input their full name in incorrect casing
+# Use the '.strip()' and '.lower()' method to remove unecessary spaces and conver the string to lower case
+# Use the '.replace()' to replace the spaces inside the string with underscore
+# Print the modified input
