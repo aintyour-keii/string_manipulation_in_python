4b26662d40a4bf48737bbf82da4950a8a0d2a80b
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:50:33 +0800

    code completion

--- lstrip_recreation.py
+++ lstrip_recreation.py
@@ -1,4 +1,13 @@
 # Ask for user input
+user_input = input("Enter a string with leading spaces: ")
+
 # Find the first non-space character in the input
+non_space_index = 0
+while non_space_index < len(user_input) and user_input[non_space_index] == " ":
+    non_space_index += 1
+    
 # Trim the input from the first non-space character
+trimmed_input = user_input[non_space_index:]
+    
 # Print the trimmed input
+print(trimmed_input)
