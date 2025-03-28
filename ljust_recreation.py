f855f6ba0774207d9a4f7436c58e8adc335b0613
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:27:24 +0800

    code completion

--- ljust_recreation.py
+++ ljust_recreation.py
@@ -1,6 +1,13 @@
 # Ask the user to input a string
+text_input = input("Enter a string: ")
 # Ask the user for the width
+width = int(input("Enter your desired width: "))
 # Check if string needs extra spaces
-# If so, add spaces to the string, 
-# the number of spaces is equal to that of the width minus the length of the string
+if len(text_input) < width:
+    # If so, add spaces to the string, 
+    # the number of spaces is equal to that of the width minus the length of the string
+    num_of_spaces = width - len(text_input)
+    text_input += ' ' * num_of_spaces
+
 # Print the result
+print(text_input)
