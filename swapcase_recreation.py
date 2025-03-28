06a5c6691e696ea7b32a74d1676f8494e62aaa68
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:36:18 +0800

    created pseudocode

---
+++ swapcase_recreation.py
@@ -0,0 +1,5 @@
+# Define lower and upper variables containing lowercase and uppercase alphabet characters respectively
+# Create a translation table which first adds the lower and upper then the upper and lower (this will be the swapping)
+# Ask the user for a string input
+# Use the '.translate()' method and pass the translation table to it to swap the case of the string
+# Print the input
