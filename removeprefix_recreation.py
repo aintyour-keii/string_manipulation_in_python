49c3f856d62d6310c00b179e5a4b065d9f2c4a92
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:54:11 +0800

    code completion

--- removeprefix_recreation.py
+++ removeprefix_recreation.py
@@ -1,5 +1,11 @@
 # Ask for the user input
+user_input = input("Enter a string: ")
 # Ask for the prefix to be removed
+prefix = input("Enter the prefix to be removed: ")
 # Check if the string starts with the prefix
-# If it does, remove the prefix and print the result
-# If it doesn't, print the string unmodified
+if user_input.startswith(prefix):
+    # If it does, remove the prefix and print the result
+    print(user_input[len(prefix):])
+else:
+    # If it doesn't, print the string unmodified
+    print(user_input)
