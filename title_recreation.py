b1a06da2757c3fe1ef8c72b9d9257094b9bb8ecb
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 22:53:51 +0800

    pseudocode changes

--- title_recreation.py
+++ title_recreation.py
@@ -2,5 +2,6 @@
 # Split the string
 # Create a variable to store the modified string
 # Loop throught the parts of the string
-# Use '.capitalize()' on each part and add it onto the modified string variable
+# Check if the modified string is still empty, then add the first word after using the '.capitalize()' on it
+# if the modified string is not empty, Use '.capitalize()' on each part and add it onto the modified string after a space
 # After looping, print the modified string variable
