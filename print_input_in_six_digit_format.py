848f8ecf34bbcf02a2ce2fbc1cc75e37c2f0a351
aintyour-keii <klynepascual.edu@gmail.com>
2025-03-28 21:20:41 +0800

    changing input type to string and using zfill

--- print_input_in_six_digit_format.py
+++ print_input_in_six_digit_format.py
@@ -2,5 +2,5 @@
 number = int(input("Enter a number between 0 and 1000: "))
 # Check if the number is valid
 if 0 <= number <= 1000:
-# If valid, use the '.zfill()' method to make the input into a 6 digit number then print the modified input
-    print("Valid Input")
+    # If valid, use the '.zfill()' method to make the input into a 6 digit number then print the modified input
+    number = str(number).zfill(6)
